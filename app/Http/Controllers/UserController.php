a030eee0a81fbd9f55d66dd045bdcc8e391fbf04
Arina Idam-Syuryun <idamsyuryunarina@gmail.om>
2024-02-15 16:38:52 +0300

    LoginRequest and RegistrationRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Session\Session;
