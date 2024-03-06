a7ad270530ace6f5e97f0eeb477ba011f684bb4f
Arina Idam-Syuryun <idamsyuryunarina@gmail.om>
2024-03-06 17:53:08 +0300

    расширение http для PHP

--- app/Http/Controllers/AccountSettingsController.php
+++ app/Http/Controllers/AccountSettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AccountSettingsRequest;
 use App\Models\Information;
 use Illuminate\Support\Facades\Auth;
+use http\Exception\InvalidArgumentException;
 
 class AccountSettingsController extends Controller
 {
