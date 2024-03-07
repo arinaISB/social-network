1098340843eeea775d7f06cc4cb138f7f1565bb6
Arina Idam-Syuryun <idamsyuryunarina@gmail.om>
2024-03-07 15:39:20 +0300

    методы внутри компонента vuejs

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,11 +1,37 @@
 <script>
+import SinglePostComponent from "./SinglePostComponent.vue";
 export default {
-    name: "PostAndComponent"
+    name: "PostAndComponent",
+
+    components: {
+        SinglePostComponent
+    },
+
+    data() {
+        return {
+            'name' : 'Vasya'
+        }
+    },
+
+    methods: {
+        sayHello() {
+            console.log('hello');
+        },
+        sayHi() {
+            console.log('hi');
+        },
+    },
 }
 </script>
 
 <template>
-    <div>111</div>
+    <div>
+        <single-post-component></single-post-component>
+<!--        <SinglePostComponent></SinglePostComponent>можно и так-->
+    </div>
+    <div>Name: {{ name }}</div>
+    <button @click="sayHello">Hellllo</button>
+
 </template>
 
 <style scoped>
---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "SinglePostComponent"
+}
+</script>
+
+<template>
+    <div>1ыштпду</div>
+</template>
+
+<style scoped>
+
+</style>
